61623ca9c1ea90e79d36ea507dd3046046aa5932
ANDY-SL <842378991@qq.com>
2015-08-15 18:59:39 +0800

    1-23

--- CSExercises/Ex07-15/Ex10.cs
+++ CSExercises/Ex07-15/Ex10.cs
@@ -10,24 +10,21 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            Console.Write("plz input 4 values x1 x2 y1 y2:\n");
+            Console.Write("plz input 4 values x1 y1 x2 y2:\n");
             int x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("x1={0}", x1);
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("x2={0}", x2);
             int y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("y1={0}", y1);
+            int x2 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("y2={0}", y2);
+            Console.Write("the two points you entered are ({0},{1}),({2},{3})\n", x1, y1, x2, y2);
             //int d=(x2-x1)^2+(y2-y1)^2;
           //  double s = Math.Sqrt(d);
             //Console.WriteLine("the distance is {0}",s);
             int x = x2 - x1;
-            double  x_square = Math.Pow(x,2);
+            int  x_square = x*x;
             int y = y2 - y1;
-            double y_square = Math.Pow(y, 2);
-            double s = Math.Sqrt(x + y);
-            System.Console.WriteLine(x_square);
+            int y_square = y*y;
+            double s = Math.Sqrt(x_square + y_square);
+            Console.WriteLine("the distance is {0:0.00}",s);
             
         }
     }
