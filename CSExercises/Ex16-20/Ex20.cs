94353bd0ca29fa9c1759073a0c08e719fd69ad8c
ANDY-SL <842378991@qq.com>
2015-08-17 13:52:05 +0800

    1-43

--- CSExercises/Ex16-20/Ex20.cs
+++ CSExercises/Ex16-20/Ex20.cs
@@ -10,7 +10,8 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            int TV = 0, DVD = 0, MP3 = 0,price=0;
+            int TV = 0, DVD = 0, MP3 = 0;
+            double price = 0;
             Console.Write("PRODUCT \tPRICE\n");
             Console.Write("TV     \t        $900\n");
             Console.Write("DVD    \t        $500\n");
@@ -23,10 +24,19 @@ namespace CSExercises
             MP3 = Convert.ToInt32(Console.ReadLine());
             price = TV * 900 + DVD * 500 + MP3 * 700;
             if (price <= 5000)
+                
                 Console.Write("the total price of your order is {0:c}\n", price);
-            else if (price > 5000 && price <= 10000) 
-                 Console.Write("the total price of your order is {0:c}\n", price * 0.9);
-            else  Console.Write("the total price of your order is {0:c}\n", price*0.95);
+
+            else if (price > 5000 && price <= 10000)
+            {
+                price = (TV * 900 + DVD * 500) * 0.9 + MP3 * 700;
+                Console.Write("the total price of your order is {0:c}\n", price );
+            }
+            else
+            {
+                price = (TV * 900 + DVD * 500) * 0.85 + MP3 * 700;
+                Console.Write("the total price of your order is {0:c}\n", price );
+            }
 
             
         }
